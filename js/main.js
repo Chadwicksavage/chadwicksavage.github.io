ab0336f9f2b13ca13642d8072776c1fa0f7a2ce1
Chadwicksavage <chadwick.joshua.savage@gmail.com>
2017-10-09 22:50:56 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,33 @@
 
-/* ===========================
---------  Navigation ---------
-=============================*/
-     $('.triple_lines').click(function() {
+/*----------------------
+       Animation
+-----------------------*/
+var lineDrawing = anime({
+  targets: 'path',
+  strokeDashoffset: [anime.setDashoffset, 0],
+  easing: 'easeInOutSine',
+  duration: 400,
+  delay: function(el, i) { return i * 250 },
+  direction: 'forwards',
+  loop: false,
+	complete: function(anim) {
+    $('.container').addClass('completed');
+    $('#mainnav').fadeIn(3500, function() {
+      
+    });
+  }
+});
+
+/*-----------------------
+    Portfolio Overlay
+------------------------*/
+$('.triple_lines').click(function() {
        $('.triple_lines').css('pointer-events', 'none');
-       var overlay_navigation = $('.nav-menu'),
+       var overlay_navigation = $('.work-menu'),
          top_bar = $('.bar-top'),
          middle_bar = $('.bar-middle'),
          bottom_bar = $('.bar-bottom');
-
+       $('.triple_lines').toggleClass('fixed_menu');
        overlay_navigation.toggleClass('overlay-active');
        if (overlay_navigation.hasClass('overlay-active')) {
 
@@ -20,11 +39,11 @@
            duration: 300,
            delay: 0,
            begin: function() {
-             $('nav ul li').velocity('transition.perspectiveLeftIn', {
+             $('.work-menu ul li').velocity('transition.perspectiveLeftIn', {
                stagger: 150,
                delay: 0,
                complete: function() {
-                 $('nav ul li a').velocity({
+                 $('.work-menu ul li a').velocity({
                    opacity: [1, 0],
                  }, {
                    delay: 10,
@@ -42,7 +61,7 @@
          middle_bar.removeClass('animate-middle-bar').addClass('animate-out-middle-bar');
          bottom_bar.removeClass('animate-bottom-bar').addClass('animate-out-bottom-bar');
          overlay_navigation.removeClass('overlay-slide-down').addClass('overlay-slide-up')
-         $('nav ul li').velocity('transition.perspectiveRightOut', {
+         $('.work-menu ul li').velocity('transition.perspectiveRightOut', {
            stagger: 150,
            delay: 0,
            complete: function() {
@@ -50,7 +69,7 @@
                delay: 0,
                duration: 300,
                complete: function() {
-                 $('nav ul li a').velocity({
+                 $('.work-menu ul li a').velocity({
                    opacity: [0, 1],
                  }, {
                    delay: 0,
